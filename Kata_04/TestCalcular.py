3c5f45dee76213d513bdffca052222140ea58b16
Christian Sanchez Leon <chsanleo@gmail.com>
2023-03-11 00:40:21 +0100

    add test for all methods

--- Kata_04/TestCalcular.py
+++ Kata_04/TestCalcular.py
@@ -15,12 +15,41 @@ class TestCalcular(unittest.TestCase):
         print("setUp() -> OK")
 
     def test_suma(self):
-        """
-        Test that it can sum a list of integers
-        """
         result = Calc.sumar(8, 78)
         self.assertEqual(result, 86)
         print("test_suma -> OK")
+
+    def test_resta(self):
+        result = Calc.restar(30, 15)
+        self.assertEqual(result, 15)
+
+    def test_doblar(self):
+        result = Calc.doblar(15)
+        self.assertEqual(result, 30)
+
+    def test_multiplicar(self):
+        result = Calc.multiplicar(9, 9)
+        self.assertEqual(result, 81)
+
+    def test_dividir(self):
+        result = Calc.dividir(81, 9)
+        self.assertEqual(result, 9)
+
+    def test_cuadrado(self):
+        result = Calc.cuadrado(11)
+        self.assertEqual(result, 121)
+
+    def test_raiz(self):
+        result = Calc.raiz(121)
+        self.assertEqual(result, 11)
+
+    def test_esPar_par(self):
+        result = Calc.es_par(99230390)
+        self.assertTrue(result)
+
+    def test_esPar_impar(self):
+        result = Calc.es_par(778343)
+        self.assertFalse(result)
 
     def tearDown(self):
         print("tearDown() -> OK")
